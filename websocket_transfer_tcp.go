0bd071c1931905760afd6bc99f17ca974ecfdeab
Jane <iwof920125@qq.com>
2018-08-17 16:39:31 +0800

    初测通过

--- websocket_transfer_tcp.go
+++ websocket_transfer_tcp.go
@@ -4,35 +4,41 @@ import (
     "io"
     "log"
     "net"
-    "os"
-    "fmt"
+    // "os"
+    // "fmt"
     "bufio"
     "bytes"
     "strconv"
     "strings"
     "crypto/sha1"
-    "runtime"
     "encoding/base64"
     "encoding/binary"
+    "runtime"
     "net/http"
 )
 
-type WebSocket struct {
+type SocketType byte
+const (
+    EWebSocket SocketType = iota   // value --> 0
+    ETcpSocket                     // value --> 1
+)
+
+type TcpSocket struct {
     Listener    net.Listener
     Clients        []*Client
+    socketType     SocketType   
 }
 
-var num int
-var new_addr string
 type Client struct {
     Conn         net.Conn
     Nickname    string
     Shook        bool
-    Server        *WebSocket
+    Server        *TcpSocket
     Id            int
     TcpConn      net.Conn
     WebsocketType   int
     Num             int
+    socketType     SocketType   
 }
 
 type Msg struct {
@@ -46,61 +52,81 @@ func (self *Client) Release() {
     self.Conn.Close()
 }
 
-
+// 连接初始化 需要支持websocket&socket
 func (self *Client) Handle() {
+    log.Println("Handle,socketType：", self.socketType)
     defer self.Release()
+
+    if(self.socketType == EWebSocket){
     if !self.Handshake() {
-        // handshak err , del this conn
+            log.Fatalln("Handshake err , del this conn")
         return
+    }
     }
 
     // connect to another server for tcp
     if !self.ConnTcpServer(){
-        // can not connect to the other server , release
+        log.Fatalln("can not connect to the other server , release")
         return
     }
     num = num + 1
-    log.Print("now connect num : ", num)
+    log.Println("now connect num : ", num)
     self.Num = num
     go self.Read()
     self.ReadTcp()
 }
 
-func (self *Client) Read() {
+func (self *Client) ReadFromTcpSocket() {
+    request := make([]byte, 128)
+    for {
+		read_len, err := self.Conn.Read(request)
+
+		if err != nil {
+			log.Println(err)
+			break
+		}
+
+		if read_len == 0 {
+			break // connection already closed by client
+		}  else {
+			buf := request[:read_len]
+            log.Println("rec from the client(",self.Num,")", string(buf))
+            self.TcpConn.Write(buf)
+		}
+        request = make([]byte, 128) // clear last read content
+	}
+}
+
+func (self *Client) ReadFromWebSocket() {
     var (
         buf     []byte
-        err     error
-        rsv     byte
-        opcode    byte
-        mask    byte
         mKey    []byte
         length    uint64
         l        uint16
-        payload    byte
     )
     for {
         buf = make([]byte, 2)
-        _, err = io.ReadFull(self.Conn, buf)
+        _, err := io.ReadFull(self.Conn, buf)
         if err != nil {
+            log.Println(err)
             self.Release()
             break
         }
         //fin = buf[0] >> 7
         //if fin == 0 {
         //}
-
-        rsv = (buf[0] >>4) &0x7
+        rsv := (buf[0] >>4) &0x7
         // which must be 0
         if rsv != 0{
-            log.Print("Client send err msg:",rsv,", disconnect it")
+            log.Println("Client send err msg:",rsv,", disconnect it")
             self.Release()
             break
         }
 
-        opcode = buf[0] & 0xf
+        opcode := buf[0] & 0xf
         // opcode   if 8 then disconnect
         if opcode == 8 {
-            log.Print("CLient want close Connection")
+            log.Println("CLient want close Connection")
             self.Release()
             break
         }
@@ -109,27 +135,27 @@ func (self *Client) Read() {
         // if client send by binary should return binary (especially for Egret)
         self.WebsocketType = int(opcode)
 
-        mask = buf[1] >> 7
+        mask := buf[1] >> 7
         // the translate may have mask 
 
-        payload = buf[1] & 0x7f
+        payload := buf[1] & 0x7f
         // if length < 126 then payload mean the length
         // if length == 126 then the next 8bit mean the length
         // if length == 127 then the next 64bit mean the length
         switch {
-        case payload < 126:
-            length = uint64(payload)
+            case payload < 126:
+                length = uint64(payload)
 
-        case payload == 126:
-            buf = make([]byte, 2)
-            io.ReadFull(self.Conn, buf)
-            binary.Read(bytes.NewReader(buf), binary.BigEndian, &l)
-            length = uint64(l)
+            case payload == 126:
+                buf = make([]byte, 2)
+                io.ReadFull(self.Conn, buf)
+                binary.Read(bytes.NewReader(buf), binary.BigEndian, &l)
+                length = uint64(l)
 
-        case payload == 127:
-            buf = make([]byte, 8)
-            io.ReadFull(self.Conn, buf)
-            binary.Read(bytes.NewReader(buf), binary.BigEndian, &length)
+            case payload == 127:
+                buf = make([]byte, 8)
+                io.ReadFull(self.Conn, buf)
+                binary.Read(bytes.NewReader(buf), binary.BigEndian, &length)
         }
         if mask == 1 {
             mKey = make([]byte, 4)
@@ -141,13 +167,24 @@ func (self *Client) Read() {
             for i, v := range buf {
                 buf[i] = v ^ mKey[i % 4]
             }
-            //fmt.Print("mask", mKey)
+            //fmt.Println("mask", mKey)
         }
-        log.Print("rec from the client(",self.Num,")", string(buf))
+              
+        log.Println("rec from the client(",self.Num,")", string(buf))
         self.TcpConn.Write(buf)
     }
 }
-// read from other tcp
+
+// Read 需要支持websocket&socket
+func (self *Client) Read() {
+    if(self.socketType == EWebSocket){
+        self.ReadFromWebSocket();
+    } else {
+        self.ReadFromTcpSocket();
+    } 
+}
+
+// read from server tcp
 func (self *Client) ReadTcp() {
     var (
         buf  []byte
@@ -161,19 +198,19 @@ func (self *Client) ReadTcp() {
             self.Release()
             num = num - 1
             // only 
-            log.Print("other tcp connect err", err)
-            log.Print("disconnect client :", self.Num)
-            log.Print("now have:", num)
+            log.Println("other tcp connect err", err)
+            log.Println("disconnect client :", self.Num)
+            log.Println("now have:", num)
             break
         }
-        log.Print("recv from other tcp : ", string(buf[:length]))
+        log.Println("recv from other tcp : ", string(buf[:length]))
         self.Write(buf[:length])
-        //Write to websocket
     }
 }
 
-// write to websocket
+// write 需要支持websocket&socket
 func (self *Client) Write(data []byte) bool {
+    if(self.socketType == EWebSocket){
     data_binary := new(bytes.Buffer) //which 
 
     //should be binary or string
@@ -185,7 +222,7 @@ func (self *Client) Write(data []byte) bool {
         // 10000010
         err := binary.Write(data_binary, binary.LittleEndian, data)
         if err != nil {
-            log.Print(" translate to binary err", err)
+                    log.Println(" translate to binary err", err)
         }
         length = len(data_binary.Bytes())
     }
@@ -203,7 +240,7 @@ func (self *Client) Write(data []byte) bool {
         frame = append(frame, byte(127))
         frame = append(frame, buf...)
     default:
-        log.Print("Data too large")
+                log.Println("Data too large")
         return false
     }
     if self.WebsocketType == 2 {
@@ -212,16 +249,21 @@ func (self *Client) Write(data []byte) bool {
         frame = append(frame, data...)
     }
     self.Conn.Write(frame)
-    frame = []byte{0}
+    } else {
+        self.Conn.Write([]byte(data)) 
+    }
+
+    // frame = []byte{0}
     return true
 }
 
+// 连接到TCP服务器
 func (self *Client) ConnTcpServer() bool {
 
-    conn, err := net.Dial("tcp", new_addr)
+    conn, err := net.Dial("tcp", tcpServerAddr)
 
     if(err != nil) {
-        log.Print("connect other tcp server error")
+        log.Println("connect other tcp server error")
         return false
     }
 
@@ -229,6 +271,7 @@ func (self *Client) ConnTcpServer() bool {
     return true
 }
 
+// 处理websocket 握手问题
 func (self *Client) Handshake() bool {
     if self.Shook {
         return true
@@ -239,7 +282,7 @@ func (self *Client) Handshake() bool {
     for {
         line, _, err := reader.ReadLine()
         if err != nil {
-            log.Print("Handshake err:",err)
+            log.Println("Handshake err:", err)
             return false
         }
         if len(line) == 0 {
@@ -269,26 +312,29 @@ func (self *Client) Handshake() bool {
     return true
 }
 
-func NewWebSocket(addr string) *WebSocket {
-    l, err := net.Listen("tcp", addr)
+// 新建立连接
+func NewSocket(addr string, sType SocketType) *TcpSocket {
+    log.Println("NewSocket:", addr)
+    listener, err := net.Listen("tcp", addr)
     if err != nil {
         log.Fatal(err)
-        // if cannot listen then return
     }
-    return &WebSocket{l, make([]*Client, 0)}
+    return &TcpSocket{listener, make([]*Client, 0), sType}
 }
 
-
-func (self *WebSocket) Loop() {
+// 监听客户端消息
+func (self *TcpSocket) Loop() {
     for {
         conn, err := self.Listener.Accept()
         if err != nil {
-            log.Print("client conn err:", err)
+            log.Println("client conn err:", err)
             continue
         }
         s := conn.RemoteAddr().String()
         i, _ := strconv.Atoi(strings.Split(s, ":")[1])
-        client := &Client{conn, "", false, self, i, conn, 1, num}
+        log.Println("new Client:", s)
+
+        client := &Client{conn, "", false, self, i, conn, 1, num, self.socketType}
         go client.Handle()
     }
 }
@@ -301,29 +347,25 @@ func handler(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte(num))
 }
 
+
+// ============主函数============
+var webSocketPort = "11305"
+var tcpSocketPort = "11306"
+var IPAddress = "0.0.0.0:"
+var num = 0
+var tcpServerAddr = "127.0.0.1:12306"
 func main() {
-    arg_num:=len(os.Args)
-    if arg_num < 2 {
-        fmt.Println(arg_num)
-        fmt.Print("Wrong Arguments\nxxxx xxx.xxx.xxx.xxx:xxxx\nport ip:port(for tcp)")
-        os.Exit(0)
-    }
-    num = 0
-    conn,err := net.Dial("tcp", string(os.Args[2]))
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(0)
-    }
-    new_addr = os.Args[2]
-    fmt.Println("Check Arguments Ok")
-    conn.Close()
-    port := os.Args[1]
-    ip_port:= "0.0.0.0:"+string(port)
-    ws := NewWebSocket(ip_port)
-    // listen 9051
+
+    // webSocket
+    ws := NewSocket(IPAddress + string(webSocketPort), EWebSocket)
     go  ws.Loop()
-    fmt.Println("Start Listen")
-    // listen 11181 to show num of goroutine
+
+    // tcpSocket
+    ts := NewSocket(IPAddress + string(tcpSocketPort), ETcpSocket)
+    go  ts.Loop()
+
+    log.Println("====Start Listen====")
+    // // listen 11181 to show num of goroutine
     http.HandleFunc("/", handler)
     http.ListenAndServe(":11181", nil)
 }
